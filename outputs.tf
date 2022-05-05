d4ce3b1bbd9390e06e51957e66263bf5a8edf2c7
Nic <Nicholas.J.Kozell@das.oregon.gov>
2022-05-05 15:35:19 -0700

    initial

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+# output "webhook" {
+#     description = "Generated URI for this webhook"
+#     value = azurerm_automation_webhook.webhook.uri
+#     sensitive = true
+# }
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "resource_group" {
+  default = "rg-id-dev-002"
+}
+
+variable "location" {
+  default = "westus2"
+}
+
+variable "automation_account" {
+  default = "aa-id-mfacheck-002"
+}
+
+variable "keyvault" {
+  default = "mfavault"
+}
+
+variable "tenant_id" {
+  default = "aa3f6932-fa7c-47b4-a0ce-a598cad161cf"
+}
+
