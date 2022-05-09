85eb2cfdfbbc3ac74d6ccb58abe6f1ff4ac64a8a
Nic-Kozell <95941330+Nic-Kozell@users.noreply.github.com>
2022-05-09 14:46:55 -0700

    typos

--- resources.tf
+++ resources.tf
@@ -1,7 +1,7 @@
 resource "azurerm_automation_account" "aa" {
-  name = "${var.automation_account}"
+  name = "${var.automation_account_name}"
   location = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+  resource_group_name = "${var.resource_group_name}"
   sku_name = "Basic"
   identity {
     type = "SystemAssigned"
