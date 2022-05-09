c2cf32c2e6f638e29a2f27ff4d9db09fd720d83b
Nic-Kozell <95941330+Nic-Kozell@users.noreply.github.com>
2022-05-09 13:37:00 -0700

    Update variables.tf
    adding new variables, data types and descriptions.

--- variables.tf
+++ variables.tf
@@ -1,20 +1,42 @@
-variable "resource_group" {
-  default = "rg-id-dev-002"
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
 }
 
 variable "location" {
-  default = "westus2"
+  type        = string
+  description = "Location of resource group"
 }
 
 variable "automation_account" {
-  default = "aa-id-mfacheck-01"
+  type        = string
+  description = "Name of automation account"
 }
 
-variable "keyvault" {
-  default = "mfavault"
+variable "sku_name" {
+  type        = string
+  description = "Only Basic SKU available at present"
+  default     = "Basic"
+}
+
+variable "enable_diagnostic_settings" {
+  type        = bool
+  description = "True/False value to choose whether or not to send Automation Account Diagnostic logs to a Log Analytics Workspace."
+  default     = false
+}
+
+variable "aa_diagnostic_settings" {
+    type = object({
+        name                       = string
+        log_analytics_workspace_id = string
+    })
+    description = "Configuration details for the diagnostic settings and destination (if deployed)."
+    default     = null
 }
 
 variable "tenant_id" {
+  type        = string
+  description = "Tenant where resources will be deployed"
   default = "aa3f6932-fa7c-47b4-a0ce-a598cad161cf"
 }
 
