9dfbd19d04aa2d5535147e4b37b2df8968b475d1
Nic-Kozell <95941330+Nic-Kozell@users.noreply.github.com>
2022-05-13 15:15:15 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "automation_account_id" {
-    description = "Automation account id"
-    value = azurerm_automation_account.aa.id
-}
+# output "automation_account_id" {
+#     description = "Automation account id"
+#     value = azurerm_automation_account.aa.id
+# }
 
-output "automation_account_identity" {
-    description = "Automation account identity principal"
-    value = ${azurerm_automation_account.aa.identity[0].principal_id}
-}
+# output "automation_account_identity" {
+#     description = "Automation account identity principal"
+#     value = ${azurerm_automation_account.aa.identity[0].principal_id}
+# }
